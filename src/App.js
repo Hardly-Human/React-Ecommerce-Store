a121e60cde4f8df388cd2f5f392cf252361fa03f
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-07-29 22:55:18 +0530

    updating our app with a shop page collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage-component.jsx";
+import ShopPage from "./pages/shop/shop.component";
 
-const HatsPage = (props) => {
-	console.log(props);
-	return (
-		<div>
-			<h1>Hats Page id : {props.match.params.hatsid}</h1>
-		</div>
-	);
-};
+// const HatsPage = (props) => {
+// 	console.log(props);
+// 	return (
+// 		<div>
+// 			<h1>Hats Page id : {props.match.params.hatsid}</h1>
+// 		</div>
+// 	);
+// };
 function App() {
 	return (
 		<div>
 			<Switch>
 				<Route exact path="/" component={HomePage} />
-				<Route exact path="/shop/hats" component={HatsPage} />
-				<Route path="/shop/hats/:hatsid" component={HatsPage} />
+				<Route exact path="/shop" component={ShopPage} />
 			</Switch>
 		</div>
 	);
